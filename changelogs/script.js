9064c9b45bc5743731c36b42b8931915c5872e1f
Panonim <106886746+ViiiBOT@users.noreply.github.com>
2023-08-16 22:39:25 +0200

    update

--- changelogs/script.js
+++ changelogs/script.js
@@ -1,11 +1,8 @@
 
 const feedbackCards = document.querySelectorAll('.feedback-card');
 
-
 feedbackCards.forEach(card => {
   card.addEventListener('click', () => {
     card.classList.toggle('active');
   });
 });
-
-
